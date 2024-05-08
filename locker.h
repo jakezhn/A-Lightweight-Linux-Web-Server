0c9ed66ff7652ca752a3f2767d62bdbd046c72b1
jakezhn <64647378+jakezhn@users.noreply.github.com>
2024-05-08 15:39:04 -0400

    refine locker.h

--- locker.h
+++ locker.h
@@ -5,105 +5,94 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-class sem
-{
+// Semaphore wrapper class for managing semaphores
+class sem {
 public:
-    sem()
-    {
-        if (sem_init(&m_sem, 0, 0) != 0)
-        {
-            throw std::exception();
+    sem(int num = 0) {
+        if (sem_init(&m_sem, 0, num) != 0) {
+            throw std::exception(); 
         }
     }
-    sem(int num)
-    {
-        if (sem_init(&m_sem, 0, num) != 0)
-        {
-            throw std::exception();
-        }
-    }
-    ~sem()
-    {
+
+    ~sem() {
         sem_destroy(&m_sem);
     }
-    bool wait()
-    {
+
+    // Wait decreases the semaphore
+    bool wait() {
         return sem_wait(&m_sem) == 0;
     }
-    bool post()
-    {
+
+    // Post increases the semaphore
+    bool post() {
         return sem_post(&m_sem) == 0;
     }
 
 private:
-    sem_t m_sem;
+    sem_t m_sem;  
 };
-class locker
-{
+
+class locker {
 public:
-    locker()
-    {
-        if (pthread_mutex_init(&m_mutex, NULL) != 0)
-        {
+    locker() {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
             throw std::exception();
         }
     }
-    ~locker()
-    {
+
+    ~locker() {
         pthread_mutex_destroy(&m_mutex);
     }
-    bool lock()
-    {
+
+    bool lock() {
         return pthread_mutex_lock(&m_mutex) == 0;
     }
-    bool unlock()
-    {
+
+    bool unlock() {
         return pthread_mutex_unlock(&m_mutex) == 0;
     }
-    pthread_mutex_t *get()
-    {
+
+    pthread_mutex_t *get() {
         return &m_mutex;
     }
 
 private:
-    pthread_mutex_t m_mutex;
+    pthread_mutex_t m_mutex; 
 };
-class cond
-{
+
+// Condition variable class for managing condition variables
+class cond {
 public:
-    cond()
-    {
-        if (pthread_cond_init(&m_cond, NULL) != 0)
-        {
+    cond() {
+        if (pthread_cond_init(&m_cond, NULL) != 0) {
             throw std::exception();
         }
     }
-    ~cond()
-    {
+
+    ~cond() {
         pthread_cond_destroy(&m_cond);
     }
-    bool wait(pthread_mutex_t *m_mutex)
-    {
-        int ret = 0;
-        ret = pthread_cond_wait(&m_cond, m_mutex);
-        return ret == 0;
+
+    bool wait(pthread_mutex_t *m_mutex) {
+        return pthread_cond_wait(&m_cond, m_mutex) == 0;
     }
-    bool timewait(pthread_mutex_t *m_mutex, struct timespec t)
-    {
-        int ret = 0;
-        ret = pthread_cond_timedwait(&m_cond, m_mutex, &t);
-        return ret == 0;
+
+    bool timewait(pthread_mutex_t *m_mutex, struct timespec t) {
+        return pthread_cond_timedwait(&m_cond, m_mutex, &t) == 0;
     }
-    bool signal()
-    {
+
+    // Signal wakes one waiting thread
+    bool signal() {
         return pthread_cond_signal(&m_cond) == 0;
     }
-    bool broadcast()
-    {
+
+    // Broadcast wakes all waiting threads
+    bool broadcast() {
         return pthread_cond_broadcast(&m_cond) == 0;
     }
 
 private:
-    pthread_cond_t m_cond;
+    pthread_cond_t m_cond; 
 };
+
 #endif
